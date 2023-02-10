b601cc87953dcb3d6c6e8ebc0769b62e2ce28241
mxy <imxyi@hotmail.com>
2023-02-10 14:56:34 +0800

    up

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  base: 'cv',
+  integrations: [mdx()],
+  markdown: {
+    // Can be 'shiki' (default), 'prism' or false to disable highlighting
+    shikiConfig: {
+      theme: 'one-dark-pro',
+    },
+  },
+});
